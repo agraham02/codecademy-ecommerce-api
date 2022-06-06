d68fc057263dfc289cde80a47280b01ef2160761
agraham02 <acgraham02@gmail.com>
2022-06-06 14:34:03 -0400

    basic products routes finished

---
+++ queries.js
@@ -0,0 +1,46 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "codecademy_e_commerce",
+    password: "postgres",
+    port: 5432
+});
+
+const getProducts = (req, res) => {
+    pool.query("SELECT * FROM product ORDER BY department_id", (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+};
+
+const getProductById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query("SELECT * FROM product WHERE id = $1", [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    });
+};
+
+const getProductsByDepartment = (req, res) => {
+    const department = req.params.department;
+
+    pool.query("SELECT product.*, department.name AS department FROM product, department WHERE product.department_id = department.id AND department.name = $1", [department], (error, results) => {
+        if (error) {
+            console.error(error);
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    })
+}
+
+module.exports = {
+    getProducts,
+    getProductById,
+    getProductsByDepartment
+};
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const server = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
+const db = require("./queries");
 
 server.get("/", (req, res) => {
-    res.send("Hello there");
+    res.send("Shop home");
 });
+
+server.get("/products", db.getProducts);
+
+server.get("/products/:id", db.getProductById);
+
+server.get("/products/departments/:department", db.getProductsByDepartment);
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
